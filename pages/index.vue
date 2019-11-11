a34257d20e1877a384266f56a72794b7486eabf0
Valgueiro <mvt@cin.ufpe.br>
2019-11-11 11:44:47 -0300

    Add button "start"

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="pt-5">
-    <h1 class="title text-center mb-5">Escolha sua população inicial de pássaros:</h1>
+  <div class="index">
+    <h1 class="title">Escolha sua população inicial de pássaros:</h1>
 
     <div class="grid">
-      <div class="bird_picker">
-
-      </div>
+      <div class="bird_picker">picker</div>
 
       <div class="remaining_points">uhuhu</div>
+    </div>
+    <div class="button-continue">
+      <div>
+        <b-button variant="success" size="lg">Começar</b-button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,18 +20,32 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+.index {
+  padding: 48px 0;
+}
+
+.title {
+  margin-bottom: 48px;
+  text-align: center;
+}
 .grid {
   display: flex;
+  height: calc(100vh - 288px);
 }
 
-.bird_picker{
-  background: red;
-  flex-grow: 4;
+.bird_picker {
+  flex-grow: 3;
+  border-right: 1px solid black;
 }
 
-.remaining_points{
-  background: blue;
-  flex-grow:1;
+.remaining_points {
+  flex-grow: 1;
 }
 
+.button-continue {
+  display: flex;
+  justify-content: flex-end;
+  box-sizing: border-box;
+  padding: 48px 96px 0 0;
+}
 </style>
