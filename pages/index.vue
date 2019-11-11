eabc6019aeff26287a2e8e79e6279a17a5d74cdd
Valgueiro <mvt@cin.ufpe.br>
2019-11-11 11:28:39 -0300

    Add inital page structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,34 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        bird_evolution
-      </h1>
-      <h2 class="subtitle">
-        Bird Evolution - An interesting way of learning Genetic Algorithms
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="pt-5">
+    <h1 class="title text-center mb-5">Escolha sua população inicial de pássaros:</h1>
+
+    <div class="grid">
+      <div class="bird_picker">
+
       </div>
+
+      <div class="remaining_points">uhuhu</div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style lang="scss" scoped>
+.grid {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.bird_picker{
+  background: red;
+  flex-grow: 4;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.remaining_points{
+  background: blue;
+  flex-grow:1;
 }
 
-.links {
-  padding-top: 15px;
-}
 </style>
